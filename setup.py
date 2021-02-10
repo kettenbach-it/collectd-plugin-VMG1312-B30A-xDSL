c5a9c827adee738de610e2599c2d9104540eccaf
Volker Kettenbach <volker@kettenbach-it.de>
2021-02-10 12:47:56 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='collectd-plugin-VMG1312-xDSL',
+    version='1.0',
+    #packages=[''],
+    url='https://github.com/kettenbach-it/collectd-plugin-VMG1312-B30A-xDSL',
+    license='GPL v3',
+    author='Volker Kettenbach',
+    author_email='volker@ktnbch.de',
+    description='A collectd module written in Python for getting the xDSL status of Zyxel VMG1312 modems/routers',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    project_urls={
+        'Documentation': 'https://packaging.python.org/tutorials/distributing-packages',
+        'Source': 'https://github.com/kettenbach-it/collectd-plugin-VMG1312-B30A-xDSL',
+    }
+)
